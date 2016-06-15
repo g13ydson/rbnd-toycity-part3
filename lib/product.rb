b2ff51d6223ca4f7f13cda379bf465514e9eb4c0
Gleydson <gleydsontavares@lavid.ufpb.br>
2016-06-15 17:06:44 -0300

    add exceptions in method add_to_products

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,34 @@
 # lib/product.rb
 
 class Product
+	attr_reader :title
+
 	@@products = []
   	
   	def initialize(options={})
-  		@@products << self
+  		@title = options[:title]
+  		add_to_products
   	end
 
   	def self.all 
   		@@products
   	end
+
+  	def add_to_products
+		contains = false
+  		@@products.each do |product|
+  			if product.title == title 
+  				contains = true
+  			end
+  		end
+
+  		unless contains
+  			@@products << self
+  		else
+  			raise DuplicateProductError, "'#{@title}' already exists. (DuplicateProductError)"		
+  		end
+
+  	end
+
+
 end
